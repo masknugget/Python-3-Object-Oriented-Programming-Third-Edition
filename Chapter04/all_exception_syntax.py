81f7a58770eac181a87c7bfe5ac252c11e021975
masknugget <54693275+masknugget@users.noreply.github.com>
2019-12-19 10:39:50 +0800

    Update all_exception_syntax.py

--- Chapter04/all_exception_syntax.py
+++ Chapter04/all_exception_syntax.py
@@ -1,11 +1,11 @@
 import random
 
-some_exceptions = [ValueError, TypeError, IndexError, None]
+some_exceptions = [ValueError, TypeError, IndexError, None]    # 这里包含了None
 
 try:
     choice = random.choice(some_exceptions)
     print("raising {}".format(choice))
-    if choice:
+    if choice:                                               # 对应的是if的选择
         raise choice("An error")
 except ValueError:
     print("Caught a ValueError")
@@ -13,7 +13,7 @@ except TypeError:
     print("Caught a TypeError")
 except Exception as e:
     print("Caught some other error: %s" % (e.__class__.__name__))
-else:
+else:                                                        # 必然需要执行的，如果不触发异常
     print("This code called if there is no exception")
-finally:
+finally:                                                    # 必然执行的
     print("This cleanup code is always called")
